0afd6df9de1d31af369239fe174a95949810a76b
Michael Shi <mfshi03@gmail.com>
2024-04-12 22:31:07 -0400

    remove term

--- stable_diffusion_pytorch/diffusion.py
+++ stable_diffusion_pytorch/diffusion.py
@@ -10,9 +10,9 @@ class DepthSepLoRAConv2d(nn.Module):
         self.depthwise = nn.Conv2d(in_channels, in_channels, kernel_size, stride=stride, padding=padding, groups=in_channels)
         self.pointwise = nn.Conv2d(in_channels, out_channels, kernel_size=1)
         self.pointwise.weight.data.zero_()  # zero pointwise weights at initialization
-    
+
     def forward(self, x):
-        return self.conv(x) + self.pointwise(self.depthwise(x))
+        return self.conv(x) #+ self.pointwise(self.depthwise(x))
 
 class TimeEmbedding(nn.Module):
     def __init__(self, n_embd):

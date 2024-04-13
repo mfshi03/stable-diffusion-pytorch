3037c9b6320bc08c096e2c9ddca649aa75227d82
Michael Shi <mfshi03@gmail.com>
2024-04-13 09:49:25 -0400

    conv initialization

--- stable_diffusion_pytorch/model_loader.py
+++ stable_diffusion_pytorch/model_loader.py
@@ -69,19 +69,18 @@ def load_diffusion_new(device):
     state_dict = make_compatible(state_dict)
 
     diffusion = Diffusion().to(device)
-    '''
     new_state_dict = {}
     for key, value in state_dict.items():
-        print(key, value.shape)
-        if key.endswith('.conv.weight'):
-            new_state_dict[key] = value
+        if "conv" in key:
+            print(key)
+            new_state_dict[key.replace(".weight", ".conv.weight")] = value
             out_channels, in_channels, kernel_size, _ = value.shape
-            new_state_dict[key.replace('.conv.weight', '.depthwise.weight')] = torch.rand(in_channels, 1, kernel_size, kernel_size) 
-            new_state_dict[key.replace('.conv.weight', '.pointwise.weight')] = torch.zeros(out_channels, in_channels, 1, 1) 
+            new_state_dict[key.replace('.weight', '.depthwise.weight')] = torch.rand(in_channels, 1, kernel_size, kernel_size) 
+            new_state_dict[key.replace('.weight', '.pointwise.weight')] = torch.zeros(out_channels, in_channels, 1, 1) 
         else:
             new_state_dict[key] = value
-    '''
-    diffusion.load_state_dict(state_dict)
+
+    diffusion.load_state_dict(new_state_dict)
 
     return diffusion
 

29a13a71f0dc3f1d1e492a3b98bc5a88be4480ef
Michael Shi <mfshi03@gmail.com>
2024-04-19 11:07:26 -0400

    revert convolutions

--- stable_diffusion_pytorch/encoder.py
+++ stable_diffusion_pytorch/encoder.py
@@ -4,30 +4,19 @@ from torch.nn import functional as F
 from .decoder import AttentionBlock, ResidualBlock
 
 
-class DepthSepLoRAConv2d(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0):
-        super().__init__()
-        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding)
-        self.depthwise = nn.Conv2d(in_channels, in_channels, kernel_size, stride=stride, padding=padding, groups=in_channels)
-        self.pointwise = nn.Conv2d(in_channels, out_channels, kernel_size=1)
-        
-    def forward(self, x):
-        return self.conv(x) + self.pointwise(self.depthwise(x))
-
-
 class Encoder(nn.Sequential):
     def __init__(self):
         super().__init__(
-            DepthSepLoRAConv2d(3, 128, kernel_size=3, padding=1),
+            nn.Conv2d(3, 128, kernel_size=3, padding=1),
             ResidualBlock(128, 128),
             ResidualBlock(128, 128),
-            DepthSepLoRAConv2d(128, 128, kernel_size=3, stride=2, padding=0),
+            nn.Conv2d(128, 128, kernel_size=3, stride=2, padding=0),
             ResidualBlock(128, 256),
             ResidualBlock(256, 256),
-            DepthSepLoRAConv2d(256, 256, kernel_size=3, stride=2, padding=0),
+            nn.Conv2d(256, 256, kernel_size=3, stride=2, padding=0),
             ResidualBlock(256, 512),
             ResidualBlock(512, 512),
-            DepthSepLoRAConv2d(512, 512, kernel_size=3, stride=2, padding=0),
+            nn.Conv2d(512, 512, kernel_size=3, stride=2, padding=0),
             ResidualBlock(512, 512),
             ResidualBlock(512, 512),
             ResidualBlock(512, 512),
